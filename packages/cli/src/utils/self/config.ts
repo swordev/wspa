b0a299a271b47f132e5544a1c6715e53ca0ee2ec
Juanra GM <juanrgm724@gmail.com>
2022-09-30 00:16:31 +0200

    refactor(cli): add jsdoc

--- packages/cli/src/utils/self/config.ts
+++ packages/cli/src/utils/self/config.ts
@@ -3,12 +3,55 @@ import { ProjectManifest } from "@pnpm/types";
 export const configKey = "x-wspa" as const;
 
 export type Config = {
+  /**
+   * Config path.
+   *
+   * @example "@wspa/config/auto"
+   */
   extends?: string;
+  /**
+   * Root directory.
+   *
+   * Used by the `copyFiles` action to strip the `rootDir` path from the `outFiles`.
+   *
+   * @example "src"
+   */
   rootDir?: string;
+  /**
+   * Distributable directory.
+   *
+   * Used by the `copyFiles`, `createBinLinks` and `updateDepLinks` actions.
+   *
+   * @example "lib"
+   */
   distDir?: string;
+  /**
+   * Glob patterns of generated output files.
+   *
+   * Used by the `clean` action for deleting the files.
+   *
+   * @example ["*.tsbuildinfo", "lib"]
+   */
   outFiles?: string[];
+  /**
+   * Glob patterns of distribution files.
+   *
+   * Used by the `copyFiles` action for copying the files to the `distDir` directory.
+   *
+   * @example ["CHANGELOG.md", "src/*.json"]
+   */
   distFiles?: string[];
+  /**
+   * Overrides the package manifest.
+   *
+   * Used by the `copyFiles` and `createBinLinks` actions.
+   */
   pkgManifest?: ProjectManifest;
+  /**
+   * Keeps the "devDependencies" field in the package manifest.
+   *
+   * Used by the `copyFiles` action.
+   */
   devPkgManifestFields?: boolean;
 };
 
